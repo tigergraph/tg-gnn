1866e578f968c5f89668c60ffefdb99ef9df70c3
Chengbiao Jin <chengbiao.jin@tigergraph.com>
2026-02-25 15:27:14 -0800

    Remove unnecessary packages

--- setup/conda_setup.sh
+++ setup/conda_setup.sh
@@ -9,12 +9,14 @@
 
 if conda info | grep "active environment" | grep "base" >/dev/null; then
   conda config --add channels conda-forge
+  conda config --add channels nvidia
   conda config --add channels rapidsai
   conda config --set channel_priority strict
 else
   conda config --env --add channels conda-forge
+  conda config --env --add channels nvidia
   conda config --env --add channels rapidsai
   conda config --env --set channel_priority strict
 fi
 
-conda install -y python-abi3=3.12 cucim=26.02 cudf=26.02 cuml=26.02 cuproj=26.02 cuspatial=26.02 cuvs=26.02 cuxfilter=26.02 dask-cuda=26.02 dask-cudf=26.02 nx-cugraph=26.02 pylibraft=26.02 raft-dask=26.02 cugraph=26.02 cugraph-pyg=26.02 cudf=26.02 pylibwholegraph=26.02
+conda install -y python-abi3=3.12 cugraph=26.02 cugraph-pyg=26.02 cudf=26.02 dask-cuda=26.02 pylibwholegraph=26.02
--- setup/python_setup.sh
+++ setup/python_setup.sh
@@ -13,16 +13,11 @@ pip install torch torchvision torchaudio torch_geometric pytigergraph pytest ten
 
 pip install \
     --extra-index-url=https://pypi.anaconda.org/rapidsai-wheels-nightly/simple \
-    "cudf-cu12>=26.02.0a0,<=26.02" "dask-cudf-cu12>=26.02.0a0,<=26.02" \
-    "cuml-cu12>=26.02.0a0,<=26.02" "cugraph-cu12>=26.02.0a0,<=26.02" \
-    "nx-cugraph-cu12>=26.02.0a0,<=26.02" "cuspatial-cu12>=26.02.0a0,<=26.02" \
-    "cuproj-cu12>=26.02.0a0,<=26.02" "cuxfilter-cu12>=26.02.0a0,<=26.02" \
-    "cucim-cu12>=26.02.0a0,<=26.02" "pylibraft-cu12>=26.02.0a0,<=26.02" \
-    "raft-dask-cu12>=26.02.0a0,<=26.02" "cuvs-cu12>=26.02.0a0,<=26.02" \
-    "pylibraft-cu12>=26.02.0a0,<=26.02" "nx-cugraph-cu12>=26.02.0a0,<=26.02" \
-    "dask-cuda>=26.02.0a0,<=26.02" "cugraph-cu12>=26.02.0a0,<=26.02" \
-    "pylibwholegraph-cu12>=26.02.0a0,<=26.02" "cugraph-pyg-cu12>=26.02.0a0,<=26.02" \
-    "cudf-cu12>=26.02.0a0,<=26.02"
+    "cugraph-cu12>=26.02.0a0,<=26.02" \
+    "cugraph-pyg-cu12>=26.02.0a0,<=26.02" \
+    "cudf-cu12>=26.02.0a0,<=26.02" \
+    "dask-cuda>=26.02.0a0,<=26.02" \
+    "pylibwholegraph-cu12>=26.02.0a0,<=26.02"
 
 git clone https://github.com/rapidsai/cugraph-gnn.git
 cd cugraph-gnn/python/cugraph-pyg/
